779112b7ae26a2982062d1b4beb8242e66ee4490
Saiyam Vaid <saiyamvaid1997@gmail.com>
2022-11-29 16:15:16 +0530

    hi

---
+++ layouts/Layout.tsx
@@ -0,0 +1,69 @@
+import Head  from 'next/head';
+import React from 'react';
+import Link from 'next/link'
+import { useRouter } from 'next/router';
+
+const Layout = (props) => {
+  const router = useRouter()
+
+  const logout = async () => {
+    await fetch('http://localhost:8000/api/logout', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include',
+    })
+
+    await router.push('/login');
+}
+
+  let menu
+
+  if(!props.auth){
+
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item">
+            <Link href="/login" className='nav-link active'>Login</Link>
+          
+        </li>
+        <li className="nav-item">
+            <Link href="/register" className='nav-link active'>Register</Link>
+          
+        </li>
+      </ul>
+    )
+  }else{
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item">
+            <a href="#" className='nav-link active' onClick={logout}>Logout</a>
+          
+        </li>
+      </ul>
+    )
+  }
+    
+    return (
+        <>
+          <Head>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"/>
+          </Head>
+    
+          <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <div className="container-fluid">
+    <Link href="/" className='navbar-brand'>Home</Link>
+    
+    <div>
+      {menu}
+    </div>
+  </div>
+</nav>
+
+          <main className="form-signin">
+      {props.children}
+    </main>
+        </>
+      );
+};
+
+export default Layout;
---
+++ pages/register.tsx
@@ -0,0 +1,70 @@
+import { useRouter } from "next/router";
+import React, { SyntheticEvent, useState } from "react";
+import Layout from "../layouts/Layout";
+
+const register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter()
+
+  // let userDetails = JSON.parse(localStorage.getItem('user')) || []
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    // userDetails.push({"Name":name,"Email":email,"Password":password})
+    // console.log(userDetails)
+    // localStorage.setItem("user",JSON.stringify(userDetails))
+
+    await fetch("http://localhost:8000/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    await router.push('/login')
+  };
+
+  return (
+    <Layout>
+      <form onSubmit={submit}>
+        <h1 className="h3 mb-3 fw-normal">Please Register</h1>
+
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Name"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+    </Layout>
+  );
+};
+
+export default register;
